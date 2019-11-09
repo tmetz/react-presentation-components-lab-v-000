76a3d406f2d94ee979a3e8cb6b98249065738f71
Tammy Metz <tammy@tammymetz.com>
2019-11-09 10:27:42 +0000

    Automatically backed up by Learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -18,7 +18,7 @@ class SimpleComponent extends React.Component {
 
   render() {
     return (
-      <div onClick = {this.handleClick()}>
+      <div onClick = {this.handleClick}>
         {this.state.mood}
       </div>
     )
