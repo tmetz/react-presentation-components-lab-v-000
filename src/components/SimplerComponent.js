15915235e21cd2f5481ba6b35e7735b451723b87
Tammy Metz <tammy@tammymetz.com>
2019-11-09 10:31:27 +0000

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,8 @@
 // Code SimplerComponent Here
+
+import React, { Component } from 'react'
+
+
+const SimplerComponent = props => <div style={{backgroundColor: props.color}} onClick={props.handleClick}>I am just happy.</div>
+
+export default SimplerComponent;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+var color = "white"
+
+changeColor = () => {
+  color = "red"
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.changeColor} color={color} />
   </div>,
   document.getElementById('root')
 );
